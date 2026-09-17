9a0a820e9f5b15b0e5baa8a05db675e9fcea95bb
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-16 21:40:13 -0300

    Refactor MainActivity and update renderer initialization

--- src/Ryujinx.Android/MainActivity.cs
+++ src/Ryujinx.Android/MainActivity.cs
@@ -5,29 +5,36 @@ using Android.Content.PM;
 using Android.Widget;
 using Android.Graphics;
 using Ryujinx.Graphics.GAL;
+using Ryujinx.Graphics.GAL.Multithreading;
 
 namespace RyujinxAndroid
 {
     [Activity(Label = "Ryujinx", MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape, Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Window.AddFlags(Android.Views.WindowManagerFlags.Fullscreen | Android.Views.WindowManagerFlags.KeepScreenOn);
+
             var tv = new TextView(this);
             tv.SetTextColor(Color.Lime);
-            tv.TextSize = 12f;
+            tv.TextSize = 11f;
             tv.SetPadding(30,30,30,30);
             tv.SetTextIsSelectable(true);
+
             try
             {
                 var r = new FakeRenderer();
-                tv.Text = $"V18 MASTER - Build 72\nSWITCH OK!!!\n{r.GetType().FullName}\n{typeof(IRenderer).FullName}";
+                tv.Text = $"V18 MASTER - Build 73\nSWITCH OK!!!\nRenderer: {r.GetType().FullName}\nIRenderer: {typeof(IRenderer).FullName}\n\n#72 falhou por causa do GraphicsDebugLevel, agora vai!";
             }
-            catch(Exception ex){ tv.Text = $"V18 FAIL\n{ex}"; }
+            catch(Exception ex)
+            {
+                tv.Text = $"V18 FAIL\n{ex}";
+            }
             SetContentView(tv);
         }
+
         class FakeRenderer : IRenderer
         {
             public event EventHandler<ScreenCaptureImageInfo> ScreenCaptured { add{} remove{} }
@@ -35,6 +42,7 @@ namespace RyujinxAndroid
             public IPipeline Pipeline => null!;
             public IWindow Window => null!;
             public uint ProgramCount => 0;
+
             public void BackgroundContextAction(Action action, bool alwaysBackground = false) => action();
             public BufferHandle CreateBuffer(int size, BufferAccess access = BufferAccess.Default) => BufferHandle.Null;
             public BufferHandle CreateBuffer(nint pointer, int size) => BufferHandle.Null;
@@ -58,10 +66,14 @@ namespace RyujinxAndroid
             public ICounterEvent ReportCounter(CounterType type, EventHandler<ulong> resultHandler, float divisor, bool hostReserved) => null!;
             public void ResetCounter(CounterType type) {}
             public void WaitSync(ulong id) {}
-            public void Initialize(GraphicsDebugLevel logLevel) {}
+            public void Initialize(Ryujinx.Graphics.GAL.GraphicsDebugLevel logLevel) {}
             public void SetInterruptAction(Action<Action> interruptAction) {}
             public void Screenshot() {}
             public void Dispose() {}
+
+            // Caso seu IRenderer NÃO tenha default methods, esses 2 garantem o build verde
+            public ThreadedRenderer TryMakeThreaded(out bool threaded) { threaded = false; return null!; }
+            public void RunLoop(Action<Action> gpuLoop, ManualResetEvent executionDone) => gpuLoop(() => {});
         }
     }
 }
