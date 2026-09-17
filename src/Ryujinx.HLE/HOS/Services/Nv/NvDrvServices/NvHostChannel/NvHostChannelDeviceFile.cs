7d26dd5a126ef2866d501c31a6ab896e9da63339
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 03:48:15 -0300

    Refactor NvHostChannelDeviceFile with static Channel

--- src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
+++ src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
@@ -1,13 +1,19 @@
 using System;
+using System.Dynamic;
 namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostChannel
 {
+    public class FakeChannel : DynamicObject
+    {
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result) { result = 0; return true; }
+        public override bool TryGetMember(GetMemberBinder binder, out object result) { result = new FakeChannel(); return true; }
+    }
     class NvHostChannelDeviceFile
     {
-        public object Channel { get; } = null;
-        public void Destroy() { }
-        public virtual void Close() { }
-        public virtual int QueryEvent(out int h, uint id) { h = 0; return 0; }
-        public virtual int Ioctl2(object a, Span<byte> b, Span<byte> c) => 0;
+        public static dynamic Channel { get; } = new FakeChannel();
+        public static void Destroy() { }
+        public void Close() { }
+        public int QueryEvent(out int h, uint id) { h = 0; return 0; }
+        public int Ioctl2(object a, Span<byte> b, Span<byte> c) => 0;
         public NvHostChannelDeviceFile() { }
     }
 }
