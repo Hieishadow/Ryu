fa55e25aac0fa4cb2c258502a8708f4656245169
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 03:44:26 -0300

    Update NvHostChannelDeviceFile.cs

--- src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
+++ src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
@@ -3,6 +3,11 @@ namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostChannel
 {
     class NvHostChannelDeviceFile
     {
+        public object Channel { get; } = null;
+        public void Destroy() { }
+        public virtual void Close() { }
+        public virtual int QueryEvent(out int h, uint id) { h = 0; return 0; }
+        public virtual int Ioctl2(object a, Span<byte> b, Span<byte> c) => 0;
         public NvHostChannelDeviceFile() { }
     }
 }
