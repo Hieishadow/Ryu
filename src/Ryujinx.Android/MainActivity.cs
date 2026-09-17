6f656a9ed30e79767d0c52cbf53e3682a2257e70
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 00:36:51 -0300

    Refactor MainActivity to use RyujinxSwitch

--- src/Ryujinx.Android/MainActivity.cs
+++ src/Ryujinx.Android/MainActivity.cs
@@ -4,10 +4,12 @@ using Android.App;
 using Android.OS;
 using Android.Views;
 using Android.Content.PM;
-using Ryujinx.HLE;
+using Android.Widget;
 using Ryujinx.HLE.FileSystem;
-using Ryujinx.Common.Configuration;
 using Ryujinx.Graphics.Vulkan;
+
+// FIX do Switch ambíguo
+using RyujinxSwitch = Ryujinx.HLE.Switch;
 
 namespace RyujinxAndroid
 {
@@ -15,10 +17,10 @@ namespace RyujinxAndroid
     public class MainActivity : Activity, ISurfaceHolderCallback
     {
         SurfaceView view;
-        Switch emu;
+        RyujinxSwitch emu;
         string game;
 
-        protected override void OnCreate(Bundle b)
+        protected override void OnCreate(Bundle? b)
         {
             base.OnCreate(b);
             Window.AddFlags(WindowManagerFlags.Fullscreen | WindowManagerFlags.KeepScreenOn);
@@ -26,11 +28,10 @@ namespace RyujinxAndroid
             view.Holder.AddCallback(this);
             SetContentView(view);
 
-            // ACHA JOGO
             var dir = "/storage/emulated/0/Download/DragoNX/";
             if (Directory.Exists(dir))
             {
-                foreach(var f in Directory.GetFiles(dir))
+                foreach(var f in Directory.GetFiles(dir, "*", SearchOption.AllDirectories))
                 {
                     if(f.EndsWith(".nsp") || f.EndsWith(".xci") || f.EndsWith(".nca") || f.EndsWith(".nro"))
                     { game = f; break; }
@@ -45,25 +46,17 @@ namespace RyujinxAndroid
                 Toast.MakeText(this, "Coloca jogo em /Download/DragoNX/", ToastLength.Long).Show();
                 return;
             }
-
             try
             {
-                // File system + keys
                 var vfs = new VirtualFileSystem();
                 var keyPath = "/storage/emulated/0/Download/DragoNX/prod.keys";
-                if (File.Exists(keyPath))
-                {
-                    vfs.ImportKeys(File.ReadAllBytes(keyPath));
-                }
+                if (File.Exists(keyPath)) vfs.ImportKeys(File.ReadAllBytes(keyPath));
 
                 var renderer = new VulkanRenderer(h.Surface);
                 var fsClient = new FileSystemClient(vfs);
-
-                emu = new Switch(renderer, fsClient, fsClient);
+                emu = new RyujinxSwitch(renderer, fsClient, fsClient);
                 emu.LoadApplication(game);
-
                 new System.Threading.Thread(() => emu.Run()).Start();
-                Toast.MakeText(this, "ZELDA RODANDO JIT!", ToastLength.Short).Show();
             }
             catch(Exception e)
             {
