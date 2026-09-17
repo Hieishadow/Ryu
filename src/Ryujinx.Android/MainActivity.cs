ed7395fd63a4239b97ef2b74fd6b7342e311f987
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 10:40:27 -0300

    Update MainActivity.cs

--- src/Ryujinx.Android/MainActivity.cs
+++ src/Ryujinx.Android/MainActivity.cs
@@ -9,34 +9,51 @@ namespace DragoNX;
 [Activity(Label = "DragoNX", MainLauncher = true, Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
 public class MainActivity : Activity
 {
+    const int REQ_ALL_FILES = 190;
     const string BasePath = "/storage/emulated/0/Download/DragoNX";
-    const string KeysPath = "/storage/emulated/0/Download/DragoNX/system/prod.keys";
 
-    protected override void OnCreate(Bundle savedInstanceState)
+    protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
 
-        if (Build.VERSION.SdkInt >= BuildVersionCodes.R &&!Android.OS.Environment.IsExternalStorageManager)
+        if (Build.VERSION.SdkInt >= BuildVersionCodes.R && !Android.OS.Environment.IsExternalStorageManager)
         {
-            StartActivity(new Intent(Settings.ActionManageAppAllFilesAccessPermission, Android.Net.Uri.Parse("package:" + PackageName)));
+            try
+            {
+                var intent = new Intent(Settings.ActionManageAppAllFilesAccessPermission);
+                intent.SetData(Android.Net.Uri.Parse("package:" + PackageName));
+                StartActivityForResult(intent, REQ_ALL_FILES);
+            }
+            catch
+            {
+                StartActivityForResult(new Intent(Settings.ActionManageAllFilesAccessPermission), REQ_ALL_FILES);
+            }
             return;
         }
 
+        LaunchDrago();
+    }
+
+    protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
+    {
+        base.OnActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQ_ALL_FILES)
+        {
+            if (Android.OS.Environment.IsExternalStorageManager)
+                LaunchDrago();
+            else
+                FinishAffinity();
+        }
+    }
+
+    void LaunchDrago()
+    {
         new File(BasePath + "/system").Mkdirs();
         new File(BasePath + "/games").Mkdirs();
         new File(BasePath + "/cache").Mkdirs();
 
         var intent = new Intent(this, typeof(GameActivity));
 
-        // Pega o primeiro jogo que achar em /games
-        var gamesDir = new File(BasePath + "/games");
-        var files = gamesDir.ListFiles();
-        if (files!= null && files.Length > 0)
+        try
         {
-            intent.PutExtra("rom_path", files[0].AbsolutePath);
-        }
-
-        StartActivity(intent);
-        Finish();
-    }
-}
+           
