435946a4baed584ac268136ef9cd51da2621f231
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 11:19:18 -0300

    Refactor MainActivity for external storage access

--- src/Ryujinx.Android/MainActivity.cs
+++ src/Ryujinx.Android/MainActivity.cs
@@ -2,70 +2,43 @@ using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Provider;
-using Java.IO;
 
 namespace DragoNX;
 
 [Activity(Label = "DragoNX", MainLauncher = true, Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
 public class MainActivity : Activity
 {
-    const int REQ_ALL_FILES = 190;
-    const string BasePath = "/storage/emulated/0/Download/DragoNX";
-
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
 
-        if (Build.VERSION.SdkInt >= BuildVersionCodes.R &&!Android.OS.Environment.IsExternalStorageManager)
+        if (Build.VERSION.SdkInt >= BuildVersionCodes.R)
         {
-            try
+            if (!Android.OS.Environment.IsExternalStorageManager)
             {
                 var intent = new Intent(Settings.ActionManageAppAllFilesAccessPermission);
                 intent.SetData(Android.Net.Uri.Parse("package:" + PackageName));
-                StartActivityForResult(intent, REQ_ALL_FILES);
+                StartActivity(intent);
+                return;
             }
-            catch
-            {
-                StartActivityForResult(new Intent(Settings.ActionManageAllFilesAccessPermission), REQ_ALL_FILES);
-            }
-            return;
         }
 
-        LaunchDrago();
-    }
+        // FIX LINHA 51,52,53,59 - usando Java.IO.File completo
+        var baseDir = new Java.IO.File("/storage/emulated/0/Download/DragoNX");
+        if (!baseDir.Exists()) baseDir.Mkdirs();
+        new Java.IO.File("/storage/emulated/0/Download/DragoNX/system").Mkdirs();
+        new Java.IO.File("/storage/emulated/0/Download/DragoNX/games").Mkdirs();
+        new Java.IO.File("/storage/emulated/0/Download/DragoNX/cache").Mkdirs();
+        new Java.IO.File("/storage/emulated/0/Download/DragoNX/cache/jit").Mkdirs();
 
-    protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
-    {
-        base.OnActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQ_ALL_FILES)
+        var gamesDir = new Java.IO.File("/storage/emulated/0/Download/DragoNX/games");
+        var files = gamesDir.ListFiles();
+        var intent2 = new Intent(this, typeof(GameActivity));
+        if (files!= null && files.Length > 0)
         {
-            if (Android.OS.Environment.IsExternalStorageManager)
-                LaunchDrago();
-            else
-                FinishAffinity();
+            intent2.PutExtra("rom_path", files[0].AbsolutePath);
         }
-    }
-
-    void LaunchDrago()
-    {
-        new File(BasePath + "/system").Mkdirs();
-        new File(BasePath + "/games").Mkdirs();
-        new File(BasePath + "/cache").Mkdirs();
-
-        var intent = new Intent(this, typeof(GameActivity));
-
-        try
-        {
-            var gamesDir = new File(BasePath + "/games");
-            var files = gamesDir.ListFiles();
-            if (files!= null && files.Length > 0)
-            {
-                intent.PutExtra("rom_path", files[0].AbsolutePath);
-            }
-        }
-        catch { }
-
-        StartActivity(intent);
+        StartActivity(intent2);
         Finish();
     }
 }
