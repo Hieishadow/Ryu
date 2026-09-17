8b7c7519489974cfb7ed857585aa535a4466e2ba
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 03:51:38 -0300

    Update NvHostChannelDeviceFile.cs

--- src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
+++ src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
@@ -1,19 +1,9 @@
 using System;
-using System.Dynamic;
-namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostChannel
+namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostAsGpu
 {
-    public class FakeChannel : DynamicObject
+    class NvHostAsGpuDeviceFile
     {
-        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result) { result = 0; return true; }
-        public override bool TryGetMember(GetMemberBinder binder, out object result) { result = new FakeChannel(); return true; }
-    }
-    class NvHostChannelDeviceFile
-    {
-        public static dynamic Channel { get; } = new FakeChannel();
-        public static void Destroy() { }
+        public NvHostAsGpuDeviceFile() { }
         public void Close() { }
-        public int QueryEvent(out int h, uint id) { h = 0; return 0; }
-        public int Ioctl2(object a, Span<byte> b, Span<byte> c) => 0;
-        public NvHostChannelDeviceFile() { }
     }
 }
