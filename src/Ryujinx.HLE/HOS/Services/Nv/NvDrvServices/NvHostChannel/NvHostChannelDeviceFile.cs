367242436970276c1aae19e4b8b6df118a689611
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 04:08:57 -0300

    Refactor NvHostAsGpuDeviceFile to NvHostChannelDeviceFile

--- src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
+++ src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
@@ -1,8 +1,16 @@
-namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostAsGpu
+using System.Dynamic;
+namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostChannel
 {
-    class NvHostAsGpuDeviceFile
+    public class FakeChannel : DynamicObject
     {
-        public NvHostAsGpuDeviceFile() { }
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result) { result = 0; return true; }
+        public override bool TryGetMember(GetMemberBinder binder, out object result) { result = new FakeChannel(); return true; }
+    }
+    class NvHostChannelDeviceFile
+    {
+        public static dynamic Channel { get; } = new FakeChannel();
+        public static void Destroy() { }
         public void Close() { }
+        public NvHostChannelDeviceFile() { }
     }
 }
