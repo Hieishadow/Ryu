fa2babf1ddc06fb53520fe76b1e70769c07a7799
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 10:39:13 -0300

    Update GameActivity.cs

--- src/Ryujinx.Android/GameActivity.cs
+++ src/Ryujinx.Android/GameActivity.cs
@@ -1,25 +1,38 @@
 using Android.App;
 using Android.OS;
 using Android.Views;
+using Android.Widget;
 using Ryujinx.Ava;
-using Ryujinx.Graphics.Vulkan;
+using Ryujinx.Ava.Host;
+using System.IO;
 
 namespace DragoNX;
 
-[Activity(Label = "DragoNX", Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen", ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | Android.Content.PM.ConfigChanges.ScreenSize | Android.Content.PM.ConfigChanges.KeyboardHidden)]
+[Activity(Label = "DragoNX", Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen", 
+          ConfigurationChanges = Android.Content.PM.ConfigChanges.Orientation | 
+                                 Android.Content.PM.ConfigChanges.ScreenSize | 
+                                 Android.Content.PM.ConfigChanges.KeyboardHidden)]
 public class GameActivity : Activity
 {
-    private GameHost _host;
+    private GameHost? _host;
+    const string BasePath = "/storage/emulated/0/Download/DragoNX";
 
-    protected override void OnCreate(Bundle savedInstanceState)
+    protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
         Window.AddFlags(WindowManagerFlags.Fullscreen | WindowManagerFlags.KeepScreenOn | WindowManagerFlags.HardwareAccelerated);
 
-        var romPath = Intent.GetStringExtra("rom_path");
+        var romPath = Intent?.GetStringExtra("rom_path");
+
+        if (string.IsNullOrEmpty(romPath))
+        {
+            Toast.MakeText(this, "Sem jogo em /Download/DragoNX/games/", ToastLength.Long).Show();
+            Finish();
+            return;
+        }
 
         // Inicializa Ryujinx com Vulkan + Turnip
-        _host = new GameHost(this, romPath, "/storage/emulated/0/Download/DragoNX/system/");
+        _host = new GameHost(this, romPath, BasePath + "/system/");
         SetContentView(_host.View);
         _host.Start();
     }
@@ -27,6 +40,7 @@ public class GameActivity : Activity
     protected override void OnDestroy()
     {
         _host?.Stop();
+        _host?.Dispose();
         base.OnDestroy();
     }
 }
