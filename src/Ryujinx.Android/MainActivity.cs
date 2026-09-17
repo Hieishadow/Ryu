831a92ba44a1aa967c0b4932ff0cf129b2c5c412
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-16 21:50:30 -0300

    Refactor MainActivity by removing FakeRenderer
    Removed unused FakeRenderer class and updated text display.

--- src/Ryujinx.Android/MainActivity.cs
+++ src/Ryujinx.Android/MainActivity.cs
@@ -4,8 +4,6 @@ using Android.OS;
 using Android.Content.PM;
 using Android.Widget;
 using Android.Graphics;
-using Ryujinx.Graphics.GAL;
-using Ryujinx.Graphics.GAL.Multithreading;
 
 namespace RyujinxAndroid
 {
@@ -16,64 +14,23 @@ namespace RyujinxAndroid
         {
             base.OnCreate(savedInstanceState);
             Window.AddFlags(Android.Views.WindowManagerFlags.Fullscreen | Android.Views.WindowManagerFlags.KeepScreenOn);
-
+            
             var tv = new TextView(this);
             tv.SetTextColor(Color.Lime);
-            tv.TextSize = 11f;
+            tv.TextSize = 12f;
             tv.SetPadding(30,30,30,30);
             tv.SetTextIsSelectable(true);
-
+            
             try
             {
-                var r = new FakeRenderer();
-                tv.Text = $"V18 MASTER - Build 73\nSWITCH OK!!!\nRenderer: {r.GetType().FullName}\nIRenderer: {typeof(IRenderer).FullName}\n\n#72 falhou por causa do GraphicsDebugLevel, agora vai!";
+                var t = typeof(Ryujinx.Graphics.GAL.IRenderer);
+                tv.Text = $"V18 - Build 74\nSWITCH OK!!!\n\n{t.FullName}\nDLL: {t.Assembly.GetName().Name}\nTypes: {t.Assembly.GetTypes().Length}\n\nTrimmer OK! Count 8 virou SWITCH OK!";
             }
             catch(Exception ex)
             {
-                tv.Text = $"V18 FAIL\n{ex}";
+                tv.Text = $"FAIL 74\n{ex}";
             }
             SetContentView(tv);
-        }
-
-        class FakeRenderer : IRenderer
-        {
-            public event EventHandler<ScreenCaptureImageInfo> ScreenCaptured { add{} remove{} }
-            public bool PreferThreading => false;
-            public IPipeline Pipeline => null!;
-            public IWindow Window => null!;
-            public uint ProgramCount => 0;
-
-            public void BackgroundContextAction(Action action, bool alwaysBackground = false) => action();
-            public BufferHandle CreateBuffer(int size, BufferAccess access = BufferAccess.Default) => BufferHandle.Null;
-            public BufferHandle CreateBuffer(nint pointer, int size) => BufferHandle.Null;
-            public BufferHandle CreateBufferSparse(ReadOnlySpan<BufferRange> storageBuffers) => BufferHandle.Null;
-            public IImageArray CreateImageArray(int size, bool isBuffer) => null!;
-            public IProgram CreateProgram(ShaderSource[] shaders, ShaderInfo info) => null!;
-            public ISampler CreateSampler(SamplerCreateInfo info) => null!;
-            public ITexture CreateTexture(TextureCreateInfo info) => null!;
-            public ITextureArray CreateTextureArray(int size, bool isBuffer) => null!;
-            public bool PrepareHostMapping(nint address, ulong size) => false;
-            public void CreateSync(ulong id, bool strict) {}
-            public void DeleteBuffer(BufferHandle buffer) {}
-            public PinnedSpan<byte> GetBufferData(BufferHandle buffer, int offset, int size) => new PinnedSpan<byte>(new byte[size]);
-            public Capabilities GetCapabilities() => default;
-            public ulong GetCurrentSync() => 0;
-            public HardwareInfo GetHardwareInfo() => default;
-            public IProgram LoadProgramBinary(byte[] programBinary, bool hasFragmentShader, ShaderInfo info) => null!;
-            public void SetBufferData(BufferHandle buffer, int offset, ReadOnlySpan<byte> data) {}
-            public void UpdateCounters() {}
-            public void PreFrame() {}
-            public ICounterEvent ReportCounter(CounterType type, EventHandler<ulong> resultHandler, float divisor, bool hostReserved) => null!;
-            public void ResetCounter(CounterType type) {}
-            public void WaitSync(ulong id) {}
-            public void Initialize(Ryujinx.Graphics.GAL.GraphicsDebugLevel logLevel) {}
-            public void SetInterruptAction(Action<Action> interruptAction) {}
-            public void Screenshot() {}
-            public void Dispose() {}
-
-            // Caso seu IRenderer NÃO tenha default methods, esses 2 garantem o build verde
-            public ThreadedRenderer TryMakeThreaded(out bool threaded) { threaded = false; return null!; }
-            public void RunLoop(Action<Action> gpuLoop, ManualResetEvent executionDone) => gpuLoop(() => {});
         }
     }
 }
