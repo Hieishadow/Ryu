3c3549e8ee25b5b480228b3f50502af0be5133c8
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 10:23:16 -0300

    Update MainActivity.cs

--- src/Ryujinx.Android/MainActivity.cs
+++ src/Ryujinx.Android/MainActivity.cs
@@ -1,20 +1,42 @@
 using Android.App;
+using Android.Content;
 using Android.OS;
-using Android.Widget;
+using Android.Provider;
+using Java.IO;
 
-namespace Ryujinx.Android
+namespace DragoNX;
+
+[Activity(Label = "DragoNX", MainLauncher = true, Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
+public class MainActivity : Activity
 {
-    [Activity(Label = "DragoNX", MainLauncher = true, Exported = true)]
-    public class MainActivity : Activity
+    const string BasePath = "/storage/emulated/0/Download/DragoNX";
+    const string KeysPath = "/storage/emulated/0/Download/DragoNX/system/prod.keys";
+
+    protected override void OnCreate(Bundle savedInstanceState)
     {
-        protected override void OnCreate(Bundle? savedInstanceState)
+        base.OnCreate(savedInstanceState);
+
+        if (Build.VERSION.SdkInt >= BuildVersionCodes.R &&!Android.OS.Environment.IsExternalStorageManager)
         {
-            base.OnCreate(savedInstanceState);
-            var tv = new TextView(this);
-            tv.Text = "DragoNX ABRIU! Se apareceu isso, o problema era a GameActivity.";
-            tv.TextSize = 20f;
-            tv.SetPadding(40,100,40,40);
-            SetContentView(tv);
+            StartActivity(new Intent(Settings.ActionManageAppAllFilesAccessPermission, Android.Net.Uri.Parse("package:" + PackageName)));
+            return;
         }
+
+        new File(BasePath + "/system").Mkdirs();
+        new File(BasePath + "/games").Mkdirs();
+        new File(BasePath + "/cache").Mkdirs();
+
+        var intent = new Intent(this, typeof(GameActivity));
+
+        // Pega o primeiro jogo que achar em /games
+        var gamesDir = new File(BasePath + "/games");
+        var files = gamesDir.ListFiles();
+        if (files!= null && files.Length > 0)
+        {
+            intent.PutExtra("rom_path", files[0].AbsolutePath);
+        }
+
+        StartActivity(intent);
+        Finish();
     }
 }
