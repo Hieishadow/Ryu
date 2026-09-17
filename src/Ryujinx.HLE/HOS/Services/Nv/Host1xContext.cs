dffd49fc661187864c2abbd2b0dadf3f69addc8f
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 03:26:24 -0300

    Refactor Host1xContext and add FakeHost1xDevice

--- src/Ryujinx.HLE/HOS/Services/Nv/Host1xContext.cs
+++ src/Ryujinx.HLE/HOS/Services/Nv/Host1xContext.cs
@@ -1,12 +1,27 @@
 using System;
+using System.Dynamic;
 namespace Ryujinx.HLE.HOS.Services.Nv
 {
-    class Host1xContext : IDisposable
+    public class FakeHost1xDevice : DynamicObject
     {
-        public object Smmu { get; }
-        public object MemoryAllocator { get; }
-        public object Host1x { get; }
-        public Host1xContext(object gpu, ulong pid) { }
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result) { result = 0; return true; }
+        public void RegisterDevice(object a, object b) { }
+        public void Dispose() { }
+        public object CreateContext(object a) => 0;
+        public object DestroyContext(object a) => 0;
+        public object Map(object a, object b, object c) => 0;
+        public object Unmap(object a, object b) => 0;
+        public object AllocateRange(object a, object b) => 0;
+        public object GetFreeAddress(object a) => 0;
+        public object Submit(object a) => 0;
+    }
+
+    internal class Host1xContext : IDisposable
+    {
+        public dynamic Smmu { get; } = new FakeHost1xDevice();
+        public dynamic MemoryAllocator { get; } = new FakeHost1xDevice();
+        public dynamic Host1x { get; } = new FakeHost1xDevice();
+        public Host1xContext(dynamic gpu, ulong pid) { }
         public void Dispose() { }
     }
 }
