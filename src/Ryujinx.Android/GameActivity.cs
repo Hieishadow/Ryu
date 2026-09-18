c2e99c981db059752123e84d9e81eaf5659e3af7
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-17 22:23:11 -0300

    Refactor GameActivity and update surface handling

--- src/Ryujinx.Android/GameActivity.cs
+++ src/Ryujinx.Android/GameActivity.cs
@@ -1,6 +1,7 @@
 using Android.App;
 using Android.OS;
 using Android.Views;
+using Android.Graphics;
 using Ryujinx.HLE;
 using Ryujinx.HLE.FileSystem;
 using Ryujinx.HLE.HOS.SystemState;
@@ -12,16 +13,17 @@ using LibHac.Tools.FsSystem;
 using Silk.NET.Vulkan;
 using System;
 using System.IO;
+using Java.Lang;
 
 namespace DragoNX;
 
 [Activity(Label = "DragoNX", Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
-public class GameActivity : Activity, ISurfaceHolderCallback
+public class GameActivity : Activity
 {
     string romPath = "";
-    ISurfaceHolder? holder;
     SurfaceView? surfaceView;
-    Android.Widget.TextView? log;
+    ISurfaceHolder? holder;
+    Android.Widget.TextView? logView;
 
     protected override void OnCreate(Bundle? savedInstanceState)
     {
@@ -30,71 +32,75 @@ public class GameActivity : Activity, ISurfaceHolderCallback
 
         romPath = Intent?.GetStringExtra("rom_path") ?? "/storage/emulated/0/Download/DragoNX/games/game.nsp";
 
-        log = new Android.Widget.TextView(this){ Text = $"DragoNX #245\n{Path.GetFileName(romPath)}" };
-        log.Gravity = GravityFlags.Center;
-        SetContentView(log);
+        logView = new Android.Widget.TextView(this){ Text = $"DragoNX #246\n{Path.GetFileName(romPath)}" };
+        logView.Gravity = GravityFlags.Center;
+        SetContentView(logView);
 
         surfaceView = new SurfaceView(this);
         holder = surfaceView.Holder;
-        holder.AddCallback(this);
+        holder.AddCallback(new SurfaceCallback(this));
     }
 
-    public void SurfaceCreated(ISurfaceHolder holder)
+    class SurfaceCallback : Java.Lang.Object, ISurfaceHolderCallback
     {
-        log!.Text = "Surface criado, iniciando Vulkan...";
-        
-        new System.Threading.Thread(() => {
-            try {
-                var vfs = VirtualFileSystem.CreateInstance();
-                
-                // CRIA VULKAN COM A JANELA REAL DO ANDROID
-                var gpu = VulkanRenderer.Create("DragoNX", (instance, vk) => {
-                    var ci = new AndroidSurfaceCreateInfoKHR{
-                        SType = StructureType.AndroidSurfaceCreateInfoKhr,
-                        Window = holder.Surface!.Handle
-                    };
-                    vk.CreateAndroidSurface(instance, ci, null, out var surf).CheckResult();
-                    return surf;
-                }, () => new[] { "VK_KHR_surface", "VK_KHR_android_surface" });
+        GameActivity act;
+        public SurfaceCallback(GameActivity a){ act = a; }
 
-                var audio = new DummyHardwareDeviceDriver();
-                var prop = typeof(HleConfiguration).GetProperty("UserChannelPersistence")!;
-                var userChannel = Activator.CreateInstance(prop.PropertyType, true)!;
-                var memConfig = (MemoryConfiguration)Activator.CreateInstance(typeof(MemoryConfiguration), true)!;
+        public void SurfaceCreated(ISurfaceHolder holder)
+        {
+            act.RunOnUiThread(() => act.logView!.Text = "Surface criado, iniciando Vulkan...");
+            
+            new System.Threading.Thread(() => {
+                try {
+                    var vfs = VirtualFileSystem.CreateInstance();
+                    var gpu = VulkanRenderer.Create("DragoNX", (instance, vk) => {
+                        var ci = new AndroidSurfaceCreateInfoKHR{
+                            SType = StructureType.AndroidSurfaceCreateInfoKhr,
+                            Window = holder.Surface!.Handle
+                        };
+                        vk.CreateAndroidSurface(instance, ci, null, out var surf).CheckResult();
+                        return surf;
+                    }, () => new[] { "VK_KHR_surface", "VK_KHR_android_surface" });
 
-                var hleConfigObj = new HleConfiguration(
-                    memConfig, SystemLanguage.AmericanEnglish, RegionCode.USA, VSyncMode.Switch,
-                    true, true, 1, true, IntegrityCheckLevel.None, 0, 0, "UTC",
-                    MemoryManagerMode.SoftwarePageTable, true, AspectRatio.Fixed16x9, 1f, false, "",
-                    MultiplayerMode.Disabled, false, "", "", false, 0, false, 60, null
-                );
-                var configure = typeof(HleConfiguration).GetMethod("Configure")!;
-                var hleConfig = (HleConfiguration)configure.Invoke(hleConfigObj, new object[]{ vfs, null!, null!, null!, userChannel, gpu, audio, null! })!;
+                    var audio = new DummyHardwareDeviceDriver();
+                    var prop = typeof(HleConfiguration).GetProperty("UserChannelPersistence")!;
+                    var userChannel = Activator.CreateInstance(prop.PropertyType, true)!;
+                    var memConfig = (MemoryConfiguration)Activator.CreateInstance(typeof(MemoryConfiguration), true)!;
 
-                var device = new Ryujinx.HLE.Switch(hleConfig);
-                bool ok = device.LoadNsp(romPath);
-                
-                RunOnUiThread(() => {
-                    if (!ok) { log!.Text = $"LoadNsp falhou\n{romPath}"; return; }
-                    SetContentView(surfaceView);
-                });
+                    var hleConfigObj = new HleConfiguration(
+                        memConfig, SystemLanguage.AmericanEnglish, RegionCode.USA, VSyncMode.Switch,
+                        true, true, 1, true, IntegrityCheckLevel.None, 0, 0, "UTC",
+                        MemoryManagerMode.SoftwarePageTable, true, AspectRatio.Fixed16x9, 1f, false, "",
+                        MultiplayerMode.Disabled, false, "", "", false, 0, false, 60, null
+                    );
+                    var configure = typeof(HleConfiguration).GetMethod("Configure")!;
+                    var hleConfig = (HleConfiguration)configure.Invoke(hleConfigObj, new object[]{ vfs, null!, null!, null!, userChannel, gpu, audio, null! })!;
 
-                if (ok) {
-                    while (true) {
-                        device.ProcessFrame();
-                        device.PresentFrame(() => {});
+                    var device = new Ryujinx.HLE.Switch(hleConfig);
+                    bool ok = device.LoadNsp(act.romPath);
+                    
+                    act.RunOnUiThread(() => {
+                        if (!ok) { act.logView!.Text = $"LoadNsp falhou\n{act.romPath}"; return; }
+                        act.SetContentView(act.surfaceView);
+                    });
+
+                    if (ok) {
+                        while (true) {
+                            device.ProcessFrame();
+                            device.PresentFrame(() => {});
+                        }
                     }
+                } catch (System.Exception ex) {
+                    act.RunOnUiThread(() => {
+                        var tv = new Android.Widget.TextView(act);
+                        tv.Text = "CRASH RENDER:\n" + ex.ToString();
+                        act.SetContentView(tv);
+                    });
                 }
-            } catch (Exception ex) {
-                RunOnUiThread(() => {
-                    var tv = new Android.Widget.TextView(this);
-                    tv.Text = "CRASH RENDER:\n" + ex.ToString();
-                    SetContentView(tv);
-                });
-            }
-        }).Start();
-    }
+            }).Start();
+        }
 
-    public void SurfaceChanged(ISurfaceHolder h, Format f, int w, int ht) {}
-    public void SurfaceDestroyed(ISurfaceHolder h) {}
+        public void SurfaceChanged(ISurfaceHolder holder, Format format, int width, int height) {}
+        public void SurfaceDestroyed(ISurfaceHolder holder) {}
+    }
 }
