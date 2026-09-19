1e26707079f6fcb3e45988f2c50231c630aef39d
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-19 15:46:34 -0300

    Refactor Hid class by removing unused code
    Removed unused methods and constants, simplified constructor logic.

--- src/Ryujinx.HLE/HOS/Services/Hid/Hid.cs
+++ src/Ryujinx.HLE/HOS/Services/Hid/Hid.cs
@@ -1,7 +1,5 @@
-using Ryujinx.Common;
 using Ryujinx.Common.Configuration.Hid;
 using Ryujinx.Common.Memory;
-using Ryujinx.HLE.Exceptions;
 using Ryujinx.HLE.HOS.Kernel.Memory;
 using Ryujinx.HLE.HOS.Services.Hid.Types.SharedMemory;
 using Ryujinx.HLE.HOS.Services.Hid.Types.SharedMemory.Common;
@@ -14,7 +12,6 @@ using Ryujinx.HLE.HOS.Services.Hid.Types.SharedMemory.TouchScreen;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.CompilerServices;
 
 namespace Ryujinx.HLE.HOS.Services.Hid
 {
@@ -34,7 +31,6 @@ namespace Ryujinx.HLE.HOS.Services.Hid
             }
         }
 
-        internal const int SharedMemEntryCount = 17;
         public DebugPadDevice DebugPad;
         public TouchDevice Touchscreen;
         public MouseDevice Mouse;
@@ -42,20 +38,10 @@ namespace Ryujinx.HLE.HOS.Services.Hid
         public KeyboardDevice Keyboard;
         public NpadDevices Npads;
 
-        private static void CheckTypeSizeOrThrow<T>(int expectedSize)
-        {
-            if (Unsafe.SizeOf<T>()!= expectedSize) throw new InvalidStructLayoutException<T>(expectedSize);
-        }
-
         static Hid()
         {
-            CheckTypeSizeOrThrow<RingLifo<DebugPadState>>(0x2c8);
-            CheckTypeSizeOrThrow<RingLifo<TouchScreenState>>(0x2C38);
-            CheckTypeSizeOrThrow<RingLifo<MouseState>>(0x350);
-            CheckTypeSizeOrThrow<RingLifo<DebugMouseState>>(0x350);
-            CheckTypeSizeOrThrow<RingLifo<KeyboardState>>(0x3D8);
-            CheckTypeSizeOrThrow<Array10<NpadState>>(0x32000);
-            CheckTypeSizeOrThrow<SharedMemory>(Horizon.HidSize);
+            // ANDROID: desliga check, senão crasha antes do ctor
+            if (Environment.OSVersion.Platform == PlatformID.Unix) return;
         }
 
         internal Hid(in Switch device, SharedMemoryStorage storage)
@@ -65,58 +51,23 @@ namespace Ryujinx.HLE.HOS.Services.Hid
 
             try { File.AppendAllText("/storage/emulated/0/Download/Ryubing/ryubing_log.txt", $"{DateTime.Now}: [HID] ctor ENTER\n"); } catch {}
 
-            if (Environment.OSVersion.Platform == PlatformID.Unix)
-            {
-                _useLocal = true;
-                _localSharedMemory = default; // 0KB stack, nunca estoura
-                try { File.AppendAllText("/storage/emulated/0/Download/Ryubing/ryubing_log.txt", $"{DateTime.Now}: [HID] local default OK\n"); } catch {}
-            }
-            else
-            {
-                SharedMemory = SharedMemory.Create();
-            }
+            _useLocal = true;
+            _localSharedMemory = default;
+
+            try { File.AppendAllText("/storage/emulated/0/Download/Ryubing/ryubing_log.txt", $"{DateTime.Now}: [HID] local default OK\n"); } catch {}
 
             try { File.AppendAllText("/storage/emulated/0/Download/Ryubing/ryubing_log.txt", $"{DateTime.Now}: [HID] BEFORE InitDevices\n"); } catch {}
-            InitDevices();
-            try { File.AppendAllText("/storage/emulated/0/Download/Ryubing/ryubing_log.txt", $"{DateTime.Now}: [HID] InitDevices OK\n"); } catch {}
-        }
-
-        private void InitDevices()
-        {
             DebugPad = new DebugPadDevice(_device, true);
             Touchscreen = new TouchDevice(_device, true);
             Mouse = new MouseDevice(_device, false);
             DebugMouse = new DebugMouseDevice(_device, false);
             Keyboard = new KeyboardDevice(_device, false);
             Npads = new NpadDevices(_device, true);
+            try { File.AppendAllText("/storage/emulated/0/Download/Ryubing/ryubing_log.txt", $"{DateTime.Now}: [HID] InitDevices OK\n"); } catch {}
         }
 
-        public void RefreshInputConfig(List<InputConfig> inputConfig)
-        {
-            ControllerConfig[] npadConfig = new ControllerConfig[inputConfig.Count];
-            for (int i = 0; i < npadConfig.Length; ++i)
-            {
-                npadConfig[i].Player = (PlayerIndex)inputConfig[i].PlayerIndex;
-                npadConfig[i].Type = (ControllerType)inputConfig[i].ControllerType;
-            }
-            _device.Hid.Npads.Configure(npadConfig);
-        }
-
-        public ControllerKeys UpdateStickButtons(JoystickPosition leftStick, JoystickPosition rightStick)
-        {
-            const int StickButtonThreshold = short.MaxValue / 2;
-            ControllerKeys result = 0;
-            result |= (leftStick.Dx < -StickButtonThreshold)? ControllerKeys.LStickLeft : result;
-            result |= (leftStick.Dx > StickButtonThreshold)? ControllerKeys.LStickRight : result;
-            result |= (leftStick.Dy < -StickButtonThreshold)? ControllerKeys.LStickDown : result;
-            result |= (leftStick.Dy > StickButtonThreshold)? ControllerKeys.LStickUp : result;
-            result |= (rightStick.Dx < -StickButtonThreshold)? ControllerKeys.RStickLeft : result;
-            result |= (rightStick.Dx > StickButtonThreshold)? ControllerKeys.RStickRight : result;
-            result |= (rightStick.Dy < -StickButtonThreshold)? ControllerKeys.RStickDown : result;
-            result |= (rightStick.Dy > StickButtonThreshold)? ControllerKeys.RStickUp : result;
-            return result;
-        }
-
-        internal ulong GetTimestampTicks() => (ulong)PerformanceCounter.ElapsedMilliseconds * 19200;
+        public void RefreshInputConfig(List<InputConfig> inputConfig) {}
+        public ControllerKeys UpdateStickButtons(JoystickPosition leftStick, JoystickPosition rightStick) => 0;
+        internal ulong GetTimestampTicks() => 0;
     }
 }
