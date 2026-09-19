90b1772b24618ac7c170bdc840b2e8765af007c1
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-19 18:11:52 -0300

    Fix loading of main.npdm for Zelda
    Updated the MetaLoaderExtensions to fix loading issues for Zelda by modifying how the main.npdm file is handled and adding a new field for 32-bit support.

--- src/Ryujinx.HLE/Loaders/Processes/Extensions/MetaLoaderExtensions.cs
+++ src/Ryujinx.HLE/Loaders/Processes/Extensions/MetaLoaderExtensions.cs
@@ -2,14 +2,13 @@ using LibHac.Common;
 using LibHac.Fs;
 using LibHac.Fs.Fsa;
 using LibHac.Loader;
-using LibHac.Tools.Fs;
-using LibHac.Tools.FsSystem;
 using System.Reflection;
 
 namespace Ryujinx.HLE.Loaders.Processes.Extensions
 {
     static class MetaLoaderExtensions
     {
+        // FIX 2009-0004 - Zelda 5984MB sem main.npdm
         public static void LoadDefault(this MetaLoader metaLoader)
         {
             try
@@ -20,6 +19,7 @@ namespace Ryujinx.HLE.Loaders.Processes.Extensions
                 t.GetField("_programId", flags)?.SetValue(metaLoader, (ulong)0x0100000000000000);
                 t.GetField("_is64Bit", flags)?.SetValue(metaLoader, true);
                 t.GetField("_mainThreadStackSize", flags)?.SetValue(metaLoader, (ulong)0x100000);
+                t.GetField("_is32Bit", flags)?.SetValue(metaLoader, false);
             }
             catch { }
         }
@@ -29,8 +29,14 @@ namespace Ryujinx.HLE.Loaders.Processes.Extensions
             try
             {
                 using UniqueRef<IFile> npdmFile = new();
-                fileSystem.OpenFile(ref npdmFile.Ref, ProcessConst.MainNpdmPath.ToU8Span(), OpenMode.Read).ThrowIfFailure();
-                metaLoader.Load(npdmFile.Get.AsStorage());
+                fileSystem.OpenFile(ref npdmFile.Ref, "/main.npdm".ToU8Span(), OpenMode.Read).ThrowIfFailure();
+
+                npdmFile.Get.GetSize(out long size);
+                byte[] data = new byte[size];
+                npdmFile.Get.Read(out _, 0, data, ReadOption.None);
+
+                // Agora Load(byte[]) - não mais Load(IStorage)
+                metaLoader.Load(data);
             }
             catch
             {
