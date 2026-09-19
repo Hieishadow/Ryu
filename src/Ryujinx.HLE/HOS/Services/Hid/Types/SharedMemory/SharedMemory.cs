f78b932a9e960139790646ab8ad20582ebdcbcfd
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-19 16:03:40 -0300

    Refactor SharedMemory class by removing comments
    Removed XML summary comments for clarity and updated the structure of the SharedMemory class.

--- src/Ryujinx.HLE/HOS/Services/Hid/Types/SharedMemory/SharedMemory.cs
+++ src/Ryujinx.HLE/HOS/Services/Hid/Types/SharedMemory/SharedMemory.cs
@@ -11,56 +11,38 @@ using System.Runtime.InteropServices;
 
 namespace Ryujinx.HLE.HOS.Services.Hid.Types.SharedMemory
 {
-    /// <summary>
-    /// Represent the shared memory shared between applications for input.
-    /// </summary>
     [StructLayout(LayoutKind.Explicit, Size = 0x40000)]
     struct SharedMemory
     {
-        /// <summary>
-        /// Debug controller.
-        /// </summary>
         [FieldOffset(0)]
         public RingLifo<DebugPadState> DebugPad;
 
-        /// <summary>
-        /// Touchscreen.
-        /// </summary>
         [FieldOffset(0x400)]
         public RingLifo<TouchScreenState> TouchScreen;
 
-        /// <summary>
-        /// Mouse.
-        /// </summary>
         [FieldOffset(0x3400)]
         public RingLifo<MouseState> Mouse;
 
-        /// <summary>
-        /// Keyboard.
-        /// </summary>
         [FieldOffset(0x3800)]
         public RingLifo<KeyboardState> Keyboard;
 
-        /// <summary>
-        /// Nintendo Pads.
-        /// </summary>
         [FieldOffset(0x9A00)]
         public Array10<NpadState> Npads;
 
-        /// <summary>
-        /// Debug mouse.
-        /// </summary>
         [FieldOffset(0x3DC00)]
         public RingLifo<DebugMouseState> DebugMouse;
-        
-        /// <summary>
-        /// Pad Condition.
-        /// </summary>
+
         [FieldOffset(0x3e200)]
         public NpadCondition Condition;
 
         public static SharedMemory Create()
         {
+            // ANDROID FIX: não cria 262KB na stack
+            if (Environment.OSVersion.Platform == PlatformID.Unix)
+            {
+                return default;
+            }
+
             SharedMemory result = new()
             {
                 DebugPad = RingLifo<DebugPadState>.Create(),
@@ -69,7 +51,7 @@ namespace Ryujinx.HLE.HOS.Services.Hid.Types.SharedMemory
                 Keyboard = RingLifo<KeyboardState>.Create(),
                 Condition = NpadCondition.Create(),
             };
-            
+
             Span<NpadState> npadsSpan = result.Npads.AsSpan();
 
             for (int i = 0; i < npadsSpan.Length; i++)
