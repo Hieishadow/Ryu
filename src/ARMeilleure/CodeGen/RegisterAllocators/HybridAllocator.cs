1ed445be3f9062b6d8d42bb4a7a408e5a607502e
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-19 02:40:03 -0300

    Replace HybridAllocator with LinearScanAllocator

--- src/ARMeilleure/CodeGen/RegisterAllocators/HybridAllocator.cs
+++ src/ARMeilleure/CodeGen/RegisterAllocators/HybridAllocator.cs
@@ -1,10 +1,16 @@
+using ARMeilleure.Common;
 using ARMeilleure.IntermediateRepresentation;
+using ARMeilleure.Translation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
 
 namespace ARMeilleure.CodeGen.RegisterAllocators
 {
     class HybridAllocator : IRegisterAllocator
     {
-        // ANDROID PATCH: Hybrid lento -> usa LinearScan rápido
         public AllocationResult RunPass(ControlFlowGraph cfg, StackAllocator stackAlloc, RegisterMasks regMasks)
         {
             return new LinearScanAllocator().RunPass(cfg, stackAlloc, regMasks);
