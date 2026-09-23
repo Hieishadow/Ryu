a8b4d5481d56882d546683f9e0a6224f201b1213
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-23 14:39:27 -0300

    Refactor Ioctl methods to use NvChannel

--- src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
+++ src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
@@ -1,5 +1,6 @@
 using System;
 using Ryujinx.Common.Logging;
+using Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostChannel.Types;
 
 namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostChannel
 {
@@ -7,60 +8,43 @@ namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostChannel
     {
         private static void FLogPresent(string s){ try{ var p="/storage/emulated/0/Download/Ryubing/ryubing_present.txt"; System.IO.File.AppendAllText(p, DateTime.Now.ToString("HH:mm:ss.fff")+" "+s+"\n"); }catch{} }
 
+        private readonly NvChannel _channel;
+
         public NvHostChannelDeviceFile(ServiceCtx context, ulong owner) : base(context, owner)
         {
             Path = "/dev/nvhost-gpu";
+            _channel = new NvChannel(context);
             FLogPresent("[GPFIFO] NvHostChannelDeviceFile CREATED");
         }
 
         public override NvInternalResult Ioctl(NvIoctl command, Span<byte> arguments)
         {
-            // command.Cmd contém o ID do ioctl (SubmitGpfifo = 0x1, etc)
-            FLogPresent($"[GPFIFO] Ioctl START cmd=0x{command.Cmd:X} type=0x{command.Type:X} args={arguments.Length}");
-            try
-            {
-                // Deixa o logger original do Ryujinx também mostrar
-                Logger.Debug?.Print(LogClass.ServiceNv, $"NvHostChannel Ioctl 0x{command.Cmd:X}");
-
-                // Por enquanto retorna Success pra não travar o jogo
-                // Depois que compilar e aparecer esse log, a gente injeta o SubmitGpfifo real
-                // chamando Context.Device.Gpu
-                return NvInternalResult.Success;
-            }
-            catch (Exception ex)
-            {
-                FLogPresent($"[GPFIFO] Ioctl EXCEPTION {ex}");
-                return NvInternalResult.InvalidState;
-            }
-            finally
-            {
-                FLogPresent("[GPFIFO] Ioctl END");
-            }
+            FLogPresent($"[GPFIFO] Ioctl START cmd=0x{command.Cmd:X} args={arguments.Length}");
+            var result = _channel.Ioctl(command, arguments);
+            FLogPresent($"[GPFIFO] Ioctl END result={result}");
+            return (NvInternalResult)result;
         }
 
         public override NvInternalResult Ioctl2(NvIoctl command, Span<byte> arguments, Span<byte> inlineInBuffer)
         {
-            FLogPresent($"[GPFIFO] Ioctl2 START cmd=0x{command.Cmd:X} args={arguments.Length} inline={inlineInBuffer.Length}");
-            var r = Ioctl(command, arguments);
-            FLogPresent("[GPFIFO] Ioctl2 END");
-            return r;
+            FLogPresent($"[GPFIFO] Ioctl2 START cmd=0x{command.Cmd:X}");
+            var result = _channel.Ioctl2(command, arguments, inlineInBuffer);
+            FLogPresent($"[GPFIFO] Ioctl2 END");
+            return (NvInternalResult)result;
         }
 
         public override NvInternalResult Ioctl3(NvIoctl command, Span<byte> arguments, Span<byte> inlineOutBuffer)
         {
-            FLogPresent($"[GPFIFO] Ioctl3 START cmd=0x{command.Cmd:X} args={arguments.Length} out={inlineOutBuffer.Length}");
-            var r = Ioctl(command, arguments);
-            FLogPresent("[GPFIFO] Ioctl3 END");
-            return r;
+            FLogPresent($"[GPFIFO] Ioctl3 START cmd=0x{command.Cmd:X}");
+            var result = _channel.Ioctl3(command, arguments, inlineOutBuffer);
+            FLogPresent($"[GPFIFO] Ioctl3 END");
+            return (NvInternalResult)result;
         }
 
         public override void Close()
         {
-            FLogPresent("[GPFIFO] Close CALLED");
+            FLogPresent("[GPFIFO] Close");
+            _channel?.Dispose();
         }
-
-        // Compatibilidade com código antigo que chamava Destroy()
-        public void Destroy() { Close(); }
-        public static void Destroy(object dummy = null) { }
     }
 }
