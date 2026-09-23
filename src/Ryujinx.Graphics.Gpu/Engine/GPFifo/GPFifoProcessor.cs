a192726f6637c209c3a145416232cb845974efe6
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-23 14:09:00 -0300

    Implement logging in GPFifoProcessor
    Added logging functionality to GPFifoProcessor for process start and end events.

--- src/Ryujinx.Graphics.Gpu/Engine/GPFifo/GPFifoProcessor.cs
+++ src/Ryujinx.Graphics.Gpu/Engine/GPFifo/GPFifoProcessor.cs
@@ -11,37 +11,21 @@ using System.Runtime.CompilerServices;
 
 namespace Ryujinx.Graphics.Gpu.Engine.GPFifo
 {
-    /// <summary>
-    /// Represents a GPU General Purpose FIFO command processor.
-    /// </summary>
     class GPFifoProcessor : IDisposable
     {
         private const int MacrosCount = 0x80;
         private const int MacroIndexMask = MacrosCount - 1;
-
         private const int LoadInlineDataMethodOffset = 0x6d;
         private const int UniformBufferUpdateDataMethodOffset = 0x8e4;
 
+        // #558 LOG
+        private static void FLogPresent(string s){ try{ var p="/storage/emulated/0/Download/Ryubing/ryubing_present.txt"; System.IO.File.AppendAllText(p, DateTime.Now.ToString("HH:mm:ss.fff")+" "+s+"\n"); }catch{} }
+
         private readonly GpuChannel _channel;
-
-        /// <summary>
-        /// Channel memory manager.
-        /// </summary>
         public MemoryManager MemoryManager => _channel.MemoryManager;
-
-        /// <summary>
-        /// Channel texture manager.
-        /// </summary>
         public TextureManager TextureManager => _channel.TextureManager;
-
-        /// <summary>
-        /// 3D Engine.
-        /// </summary>
         public ThreedClass ThreedClass => _3dClass;
 
-        /// <summary>
-        /// Internal GPFIFO state.
-        /// </summary>
         private struct DmaState
         {
             public int Method;
@@ -58,18 +42,11 @@ namespace Ryujinx.Graphics.Gpu.Engine.GPFifo
         private readonly InlineToMemoryClass _i2mClass;
         private readonly TwodClass _2dClass;
         private readonly DmaClass _dmaClass;
-
         private readonly GPFifoClass _fifoClass;
 
-        /// <summary>
-        /// Creates a new instance of the GPU General Purpose FIFO command processor.
-        /// </summary>
-        /// <param name="context">GPU context</param>
-        /// <param name="channel">Channel that the GPFIFO processor belongs to</param>
         public GPFifoProcessor(GpuContext context, GpuChannel channel)
         {
             _channel = channel;
-
             _fifoClass = new GPFifoClass(context, this);
             _3dClass = new ThreedClass(context, channel, _fifoClass);
             _computeClass = new ComputeClass(context, channel, _3dClass);
@@ -78,79 +55,73 @@ namespace Ryujinx.Graphics.Gpu.Engine.GPFifo
             _dmaClass = new DmaClass(context, channel, _3dClass);
         }
 
-        /// <summary>
-        /// Processes a command buffer.
-        /// </summary>
-        /// <param name="baseGpuVa">Base GPU virtual address of the command buffer</param>
-        /// <param name="commandBuffer">Command buffer</param>
         public void Process(ulong baseGpuVa, ReadOnlySpan<int> commandBuffer)
         {
-            for (int index = 0; index < commandBuffer.Length; index++)
+            FLogPresent($"[GPFIFO] Process START va=0x{baseGpuVa:X} len={commandBuffer.Length}");
+            try
             {
-                int command = commandBuffer[index];
-
-                ulong gpuVa = baseGpuVa + (ulong)index * 4;
-
-                if (_state.MethodCount != 0)
+                for (int index = 0; index < commandBuffer.Length; index++)
                 {
-                    if (TryFastI2mBufferUpdate(commandBuffer, ref index))
+                    int command = commandBuffer[index];
+                    ulong gpuVa = baseGpuVa + (ulong)index * 4;
+
+                    if (_state.MethodCount!= 0)
                     {
-                        continue;
+                        if (TryFastI2mBufferUpdate(commandBuffer, ref index))
+                        {
+                            continue;
+                        }
+
+                        Send(gpuVa, _state.Method, command, _state.SubChannel, _state.MethodCount <= 1);
+
+                        if (!_state.NonIncrementing)
+                        {
+                            _state.Method++;
+                        }
+
+                        if (_state.IncrementOnce)
+                        {
+                            _state.NonIncrementing = true;
+                        }
+
+                        _state.MethodCount--;
                     }
-
-                    Send(gpuVa, _state.Method, command, _state.SubChannel, _state.MethodCount <= 1);
-
-                    if (!_state.NonIncrementing)
+                    else
                     {
-                        _state.Method++;
-                    }
+                        CompressedMethod meth = Unsafe.As<int, CompressedMethod>(ref command);
 
-                    if (_state.IncrementOnce)
-                    {
-                        _state.NonIncrementing = true;
-                    }
+                        if (TryFastUniformBufferUpdate(meth, commandBuffer, index))
+                        {
+                            index += meth.MethodCount;
+                            continue;
+                        }
 
-                    _state.MethodCount--;
+                        switch (meth.SecOp)
+                        {
+                            case SecOp.IncMethod:
+                            case SecOp.NonIncMethod:
+                            case SecOp.OneInc:
+                                _state.Method = meth.MethodAddress;
+                                _state.SubChannel = meth.MethodSubchannel;
+                                _state.MethodCount = meth.MethodCount;
+                                _state.IncrementOnce = meth.SecOp == SecOp.OneInc;
+                                _state.NonIncrementing = meth.SecOp == SecOp.NonIncMethod;
+                                break;
+                            case SecOp.ImmdDataMethod:
+                                Send(gpuVa, meth.MethodAddress, meth.ImmdData, meth.MethodSubchannel, true);
+                                break;
+                        }
+                    }
                 }
-                else
-                {
-                    CompressedMethod meth = Unsafe.As<int, CompressedMethod>(ref command);
 
-                    if (TryFastUniformBufferUpdate(meth, commandBuffer, index))
-                    {
-                        index += meth.MethodCount;
-                        continue;
-                    }
-
-                    switch (meth.SecOp)
-                    {
-                        case SecOp.IncMethod:
-                        case SecOp.NonIncMethod:
-                        case SecOp.OneInc:
-                            _state.Method = meth.MethodAddress;
-                            _state.SubChannel = meth.MethodSubchannel;
-                            _state.MethodCount = meth.MethodCount;
-                            _state.IncrementOnce = meth.SecOp == SecOp.OneInc;
-                            _state.NonIncrementing = meth.SecOp == SecOp.NonIncMethod;
-                            break;
-                        case SecOp.ImmdDataMethod:
-                            Send(gpuVa, meth.MethodAddress, meth.ImmdData, meth.MethodSubchannel, true);
-                            break;
-                    }
-                }
+                _3dClass.FlushUboDirty();
             }
-
-            _3dClass.FlushUboDirty();
+            finally
+            {
+                FLogPresent("[GPFIFO] Process END");
+            }
         }
 
-        /// <summary>
-        /// Tries to perform a fast Inline-to-Memory data update.
-        /// If successful, all data will be copied at once, and <see cref="DmaState.MethodCount"/>
-        /// command buffer entries will be consumed.
-        /// </summary>
-        /// <param name="commandBuffer">Command buffer where the data is contained</param>
-        /// <param name="offset">Offset at <paramref name="commandBuffer"/> where the data is located, auto-incremented on success</param>
-        /// <returns>True if the fast copy was successful, false otherwise</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private bool TryFastI2mBufferUpdate(ReadOnlySpan<int> commandBuffer, ref int offset)
         {
@@ -158,7 +129,6 @@ namespace Ryujinx.Graphics.Gpu.Engine.GPFifo
             {
                 int availableCount = commandBuffer.Length - offset;
                 int consumeCount = Math.Min(_state.MethodCount, availableCount);
-
                 ReadOnlySpan<int> data = commandBuffer.Slice(offset, consumeCount);
 
                 if (_state.SubChannel == 0)
@@ -169,51 +139,32 @@ namespace Ryujinx.Graphics.Gpu.Engine.GPFifo
                 {
                     _computeClass.LoadInlineData(data);
                 }
-                else /* if (_state.SubChannel == 2) */
+                else
                 {
                     _i2mClass.LoadInlineData(data);
                 }
 
                 offset += consumeCount - 1;
                 _state.MethodCount -= consumeCount;
-
                 return true;
             }
-
             return false;
         }
 
-        /// <summary>
-        /// Tries to perform a fast constant buffer data update.
-        /// If successful, all data will be copied at once, and <see cref="CompressedMethod.MethodCount"/> + 1
-        /// command buffer entries will be consumed.
-        /// </summary>
-        /// <param name="meth">Compressed method to be checked</param>
-        /// <param name="commandBuffer">Command buffer where <paramref name="meth"/> is contained</param>
-        /// <param name="offset">Offset at <paramref name="commandBuffer"/> where <paramref name="meth"/> is located</param>
-        /// <returns>True if the fast copy was successful, false otherwise</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private bool TryFastUniformBufferUpdate(CompressedMethod meth, ReadOnlySpan<int> commandBuffer, int offset)
         {
             int availableCount = commandBuffer.Length - offset;
-
             if (meth.MethodAddress == UniformBufferUpdateDataMethodOffset &&
                 meth.MethodCount < availableCount &&
                 meth.SecOp == SecOp.NonIncMethod)
             {
                 _3dClass.ConstantBufferUpdate(commandBuffer.Slice(offset + 1, meth.MethodCount));
-
                 return true;
             }
-
             return false;
         }
 
-        /// <summary>
-        /// Sends a uncompressed method for processing by the graphics pipeline.
-        /// </summary>
-        /// <param name="gpuVa">GPU virtual address where the command word is located</param>
-        /// <param name="meth">Method to be processed</param>
         private void Send(ulong gpuVa, int offset, int argument, int subChannel, bool isLastCall)
         {
             if (offset < 0x60)
@@ -223,7 +174,6 @@ namespace Ryujinx.Graphics.Gpu.Engine.GPFifo
             else if (offset < 0xe00)
             {
                 offset *= 4;
-
                 switch (subChannel)
                 {
                     case 0:
@@ -252,11 +202,10 @@ namespace Ryujinx.Graphics.Gpu.Engine.GPFifo
                     _ => null,
                 };
 
-                if (state != null)
+                if (state!= null)
                 {
                     int macroIndex = (offset >> 1) & MacroIndexMask;
-
-                    if ((offset & 1) != 0)
+                    if ((offset & 1)!= 0)
                     {
                         _fifoClass.MmePushArgument(macroIndex, gpuVa, argument);
                     }
@@ -268,19 +217,12 @@ namespace Ryujinx.Graphics.Gpu.Engine.GPFifo
                     if (isLastCall)
                     {
                         _fifoClass.CallMme(macroIndex, state);
-
                         _3dClass.PerformDeferredDraws();
                     }
                 }
             }
         }
 
-        /// <summary>
-        /// Writes data directly to the state of the specified class.
-        /// </summary>
-        /// <param name="classId">ID of the class to write the data into</param>
-        /// <param name="offset">State offset in bytes</param>
-        /// <param name="value">Value to be written</param>
         public void Write(ClassId classId, int offset, int value)
         {
             switch (classId)
@@ -306,19 +248,11 @@ namespace Ryujinx.Graphics.Gpu.Engine.GPFifo
             }
         }
 
-        /// <summary>
-        /// Sets the shadow ram control value of all sub-channels.
-        /// </summary>
-        /// <param name="control">New shadow ram control value</param>
         public void SetShadowRamControl(int control)
         {
             _3dClass.SetShadowRamControl(control);
         }
 
-        /// <summary>
-        /// Forces a full host state update by marking all state as modified,
-        /// and also requests all GPU resources in use to be rebound.
-        /// </summary>
         public void ForceAllDirty()
         {
             _3dClass.ForceStateDirty();
@@ -326,9 +260,6 @@ namespace Ryujinx.Graphics.Gpu.Engine.GPFifo
             _channel.TextureManager.Rebind();
         }
 
-        /// <summary>
-        /// Perform any deferred draws.
-        /// </summary>
         public void PerformDeferredDraws()
         {
             _3dClass.PerformDeferredDraws();
