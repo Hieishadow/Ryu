5973778e38993853d513258b8ed4aa09b93a1547
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-23 15:00:01 -0300

    Refactor logging and streamline Ioctl methods

--- src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
+++ src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
@@ -1,50 +1,40 @@
 using System;
 using Ryujinx.Common.Logging;
-using Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostChannel.Types;
 
 namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostChannel
 {
     class NvHostChannelDeviceFile : NvDeviceFile
     {
-        private static void FLogPresent(string s){ try{ var p="/storage/emulated/0/Download/Ryubing/ryubing_present.txt"; System.IO.File.AppendAllText(p, DateTime.Now.ToString("HH:mm:ss.fff")+" "+s+"\n"); }catch{} }
-
+        private static void FLog(string s){ try{ System.IO.File.AppendAllText("/storage/emulated/0/Download/Ryubing/ryubing_present.txt", DateTime.Now.ToString("HH:mm:ss.fff")+" "+s+"\n"); }catch{} }
         private readonly NvChannel _channel;
 
         public NvHostChannelDeviceFile(ServiceCtx context, ulong owner) : base(context, owner)
         {
             Path = "/dev/nvhost-gpu";
-            _channel = new NvChannel(context);
-            FLogPresent("[GPFIFO] NvHostChannelDeviceFile CREATED");
+            _channel = new NvChannel(context); // agora existe construtor
+            FLog("[GPFIFO] DeviceFile CREATED com canal real");
         }
 
         public override NvInternalResult Ioctl(NvIoctl command, Span<byte> arguments)
         {
-            FLogPresent($"[GPFIFO] Ioctl START cmd=0x{command.Cmd:X} args={arguments.Length}");
-            var result = _channel.Ioctl(command, arguments);
-            FLogPresent($"[GPFIFO] Ioctl END result={result}");
-            return (NvInternalResult)result;
+            FLog($"[GPFIFO] Ioctl Raw=0x{command.RawValue:X8} Number={command.Number} Type={command.Type}");
+            
+            if (command.Number == 1) // SubmitGpfifo
+            {
+                return (NvInternalResult)CallIoctlMethod<Types.SubmitGpfifoArguments>(_channel.SubmitGpfifo, arguments);
+            }
+            else if (command.Number == 2)
+            {
+                return (NvInternalResult)CallIoctlMethod<Types.AllocGpfifoExArguments>(_channel.AllocGpfifoEx, arguments);
+            }
+            
+            FLog($"[GPFIFO] Ioctl Number={command.Number} não implementado");
+            return NvInternalResult.NotImplemented;
         }
 
-        public override NvInternalResult Ioctl2(NvIoctl command, Span<byte> arguments, Span<byte> inlineInBuffer)
-        {
-            FLogPresent($"[GPFIFO] Ioctl2 START cmd=0x{command.Cmd:X}");
-            var result = _channel.Ioctl2(command, arguments, inlineInBuffer);
-            FLogPresent($"[GPFIFO] Ioctl2 END");
-            return (NvInternalResult)result;
-        }
-
-        public override NvInternalResult Ioctl3(NvIoctl command, Span<byte> arguments, Span<byte> inlineOutBuffer)
-        {
-            FLogPresent($"[GPFIFO] Ioctl3 START cmd=0x{command.Cmd:X}");
-            var result = _channel.Ioctl3(command, arguments, inlineOutBuffer);
-            FLogPresent($"[GPFIFO] Ioctl3 END");
-            return (NvInternalResult)result;
-        }
-
-        public override void Close()
-        {
-            FLogPresent("[GPFIFO] Close");
-            _channel?.Dispose();
-        }
+        public override NvInternalResult Ioctl2(NvIoctl command, Span<byte> arguments, Span<byte> inlineInBuffer) => Ioctl(command, arguments);
+        public override NvInternalResult Ioctl3(NvIoctl command, Span<byte> arguments, Span<byte> inlineOutBuffer) => Ioctl(command, arguments);
+        public override void Close() { FLog("[GPFIFO] Close"); }
+        public void Destroy() => Close(); // corrige L597
     }
 }
