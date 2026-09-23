28e5c11528c24c3efcf05b3d7508508992b37c4c
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-23 14:32:40 -0300

    Refactor NvHostChannelDeviceFile with logging and methods

--- src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
+++ src/Ryujinx.HLE/HOS/Services/Nv/NvDrvServices/NvHostChannel/NvHostChannelDeviceFile.cs
@@ -1,33 +1,66 @@
 using System;
-using System.Dynamic;
+using Ryujinx.Common.Logging;
 
 namespace Ryujinx.HLE.HOS.Services.Nv.NvDrvServices.NvHostChannel
 {
-    public class FakeChannel : DynamicObject
+    class NvHostChannelDeviceFile : NvDeviceFile
     {
         private static void FLogPresent(string s){ try{ var p="/storage/emulated/0/Download/Ryubing/ryubing_present.txt"; System.IO.File.AppendAllText(p, DateTime.Now.ToString("HH:mm:ss.fff")+" "+s+"\n"); }catch{} }
 
-        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        public NvHostChannelDeviceFile(ServiceCtx context, ulong owner) : base(context, owner)
         {
-            FLogPresent($"[GPFIFO] FakeChannel CALL {binder.Name} args={args?.Length}");
-            result = 0;
-            return true;
+            Path = "/dev/nvhost-gpu";
+            FLogPresent("[GPFIFO] NvHostChannelDeviceFile CREATED");
         }
 
-        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        public override NvInternalResult Ioctl(NvIoctl command, Span<byte> arguments)
         {
-            FLogPresent($"[GPFIFO] FakeChannel GET {binder.Name}");
-            result = new FakeChannel();
-            return true;
-        }
-    }
+            // command.Cmd contém o ID do ioctl (SubmitGpfifo = 0x1, etc)
+            FLogPresent($"[GPFIFO] Ioctl START cmd=0x{command.Cmd:X} type=0x{command.Type:X} args={arguments.Length}");
+            try
+            {
+                // Deixa o logger original do Ryujinx também mostrar
+                Logger.Debug?.Print(LogClass.ServiceNv, $"NvHostChannel Ioctl 0x{command.Cmd:X}");
 
-    class NvHostChannelDeviceFile
-    {
-        public dynamic Channel { get; } = new FakeChannel();
-        public void Destroy() { }
+                // Por enquanto retorna Success pra não travar o jogo
+                // Depois que compilar e aparecer esse log, a gente injeta o SubmitGpfifo real
+                // chamando Context.Device.Gpu
+                return NvInternalResult.Success;
+            }
+            catch (Exception ex)
+            {
+                FLogPresent($"[GPFIFO] Ioctl EXCEPTION {ex}");
+                return NvInternalResult.InvalidState;
+            }
+            finally
+            {
+                FLogPresent("[GPFIFO] Ioctl END");
+            }
+        }
+
+        public override NvInternalResult Ioctl2(NvIoctl command, Span<byte> arguments, Span<byte> inlineInBuffer)
+        {
+            FLogPresent($"[GPFIFO] Ioctl2 START cmd=0x{command.Cmd:X} args={arguments.Length} inline={inlineInBuffer.Length}");
+            var r = Ioctl(command, arguments);
+            FLogPresent("[GPFIFO] Ioctl2 END");
+            return r;
+        }
+
+        public override NvInternalResult Ioctl3(NvIoctl command, Span<byte> arguments, Span<byte> inlineOutBuffer)
+        {
+            FLogPresent($"[GPFIFO] Ioctl3 START cmd=0x{command.Cmd:X} args={arguments.Length} out={inlineOutBuffer.Length}");
+            var r = Ioctl(command, arguments);
+            FLogPresent("[GPFIFO] Ioctl3 END");
+            return r;
+        }
+
+        public override void Close()
+        {
+            FLogPresent("[GPFIFO] Close CALLED");
+        }
+
+        // Compatibilidade com código antigo que chamava Destroy()
+        public void Destroy() { Close(); }
         public static void Destroy(object dummy = null) { }
-        public void Close() { }
-        public NvHostChannelDeviceFile() { }
     }
 }
