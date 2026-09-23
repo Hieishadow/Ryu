3c9dd2b25440313f4327922e2b08ca9d2eb02a8d
Hieishadow <104704123+Hieishadow@users.noreply.github.com>
2026-09-23 11:18:00 -0300

    Implement logging in CommandBufferPool
    Added logging functionality to track command buffer pool operations.

--- src/Ryujinx.Graphics.Vulkan/CommandBufferPool.cs
+++ src/Ryujinx.Graphics.Vulkan/CommandBufferPool.cs
@@ -2,6 +2,7 @@ using Silk.NET.Vulkan;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Threading;
 using Semaphore = Silk.NET.Vulkan.Semaphore;
 
@@ -10,10 +11,10 @@ namespace Ryujinx.Graphics.Vulkan
     class CommandBufferPool : IDisposable
     {
         public const int MaxCommandBuffers = 16;
+        private static void FLog(string s){ try{ var p="/storage/emulated/0/Download/Ryubing/ryubing_log.txt"; var d=Path.GetDirectoryName(p); if(d!=null){ try{ if(!Directory.Exists(d)) Directory.CreateDirectory(d); }catch{} } File.AppendAllText(p, DateTime.Now.ToString("HH:mm:ss.fff")+" [POOL] "+s+"\n"); }catch{} }
 
         private readonly int _totalCommandBuffers;
         private readonly int _totalCommandBuffersMask;
-
         private readonly Vk _api;
         private readonly Device _device;
         private readonly Queue _queue;
@@ -21,7 +22,6 @@ namespace Ryujinx.Graphics.Vulkan
         private readonly bool _concurrentFenceWaitUnsupported;
         private readonly CommandPool _pool;
         private readonly Thread _owner;
-
         public bool OwnedByCurrentThread => _owner == Thread.CurrentThread;
 
         private struct ReservedCommandBuffer
@@ -31,339 +31,151 @@ namespace Ryujinx.Graphics.Vulkan
             public int SubmissionCount;
             public CommandBuffer CommandBuffer;
             public FenceHolder Fence;
-
             public List<IAuto> Dependants;
             public List<MultiFenceHolder> Waitables;
-
             public void Initialize(Vk api, Device device, CommandPool pool)
             {
-                CommandBufferAllocateInfo allocateInfo = new()
-                {
-                    SType = StructureType.CommandBufferAllocateInfo,
-                    CommandBufferCount = 1,
-                    CommandPool = pool,
-                    Level = CommandBufferLevel.Primary,
-                };
-
+                CommandBufferAllocateInfo allocateInfo = new() { SType = StructureType.CommandBufferAllocateInfo, CommandBufferCount = 1, CommandPool = pool, Level = CommandBufferLevel.Primary, };
                 api.AllocateCommandBuffers(device, in allocateInfo, out CommandBuffer);
-
-                Dependants = [];
-                Waitables = [];
+                Dependants = []; Waitables = [];
             }
         }
 
         private readonly ReservedCommandBuffer[] _commandBuffers;
-
         private readonly int[] _queuedIndexes;
         private int _queuedIndexesPtr;
         private int _queuedCount;
         private int _inUseCount;
 
-        public unsafe CommandBufferPool(
-            Vk api,
-            Device device,
-            Queue queue,
-            Lock queueLock,
-            uint queueFamilyIndex,
-            bool concurrentFenceWaitUnsupported,
-            bool isLight = false)
+        public unsafe CommandBufferPool(Vk api, Device device, Queue queue, Lock queueLock, uint queueFamilyIndex, bool concurrentFenceWaitUnsupported, bool isLight = false)
         {
-            _api = api;
-            _device = device;
-            _queue = queue;
-            _queueLock = queueLock;
-            _concurrentFenceWaitUnsupported = concurrentFenceWaitUnsupported;
-            _owner = Thread.CurrentThread;
-
-            CommandPoolCreateInfo commandPoolCreateInfo = new()
-            {
-                SType = StructureType.CommandPoolCreateInfo,
-                QueueFamilyIndex = queueFamilyIndex,
-                Flags = CommandPoolCreateFlags.TransientBit |
-                        CommandPoolCreateFlags.ResetCommandBufferBit,
-            };
-
+            _api = api; _device = device; _queue = queue; _queueLock = queueLock; _concurrentFenceWaitUnsupported = concurrentFenceWaitUnsupported; _owner = Thread.CurrentThread;
+            CommandPoolCreateInfo commandPoolCreateInfo = new() { SType = StructureType.CommandPoolCreateInfo, QueueFamilyIndex = queueFamilyIndex, Flags = CommandPoolCreateFlags.TransientBit | CommandPoolCreateFlags.ResetCommandBufferBit, };
             api.CreateCommandPool(device, in commandPoolCreateInfo, null, out _pool).ThrowOnError();
-
-            // We need at least 2 command buffers to get texture data in some cases.
-            _totalCommandBuffers = isLight ? 2 : MaxCommandBuffers;
+            _totalCommandBuffers = isLight? 2 : MaxCommandBuffers;
             _totalCommandBuffersMask = _totalCommandBuffers - 1;
-
             _commandBuffers = new ReservedCommandBuffer[_totalCommandBuffers];
-
             _queuedIndexes = new int[_totalCommandBuffers];
-            _queuedIndexesPtr = 0;
-            _queuedCount = 0;
-
-            for (int i = 0; i < _totalCommandBuffers; i++)
-            {
-                _commandBuffers[i].Initialize(api, device, _pool);
-                WaitAndDecrementRef(i);
-            }
+            _queuedIndexesPtr = 0; _queuedCount = 0;
+            for (int i = 0; i < _totalCommandBuffers; i++) { _commandBuffers[i].Initialize(api, device, _pool); WaitAndDecrementRef(i); }
+            FLog($"ctor total={_totalCommandBuffers} OK #553 DIAG");
         }
 
-        public void AddDependant(int cbIndex, IAuto dependant)
-        {
-            dependant.IncrementReferenceCount();
-            _commandBuffers[cbIndex].Dependants.Add(dependant);
-        }
-
-        public void AddWaitable(MultiFenceHolder waitable)
-        {
-            lock (_commandBuffers)
-            {
-                for (int i = 0; i < _totalCommandBuffers; i++)
-                {
-                    ref ReservedCommandBuffer entry = ref _commandBuffers[i];
-
-                    if (entry.InConsumption)
-                    {
-                        AddWaitable(i, waitable);
-                    }
-                }
-            }
-        }
-
-        public void AddInUseWaitable(MultiFenceHolder waitable)
-        {
-            lock (_commandBuffers)
-            {
-                for (int i = 0; i < _totalCommandBuffers; i++)
-                {
-                    ref ReservedCommandBuffer entry = ref _commandBuffers[i];
-
-                    if (entry.InUse)
-                    {
-                        AddWaitable(i, waitable);
-                    }
-                }
-            }
-        }
-
-        public void AddWaitable(int cbIndex, MultiFenceHolder waitable)
-        {
-            ref ReservedCommandBuffer entry = ref _commandBuffers[cbIndex];
-            if (waitable.AddFence(cbIndex, entry.Fence))
-            {
-                entry.Waitables.Add(waitable);
-            }
-        }
-
-        public bool HasWaitableOnRentedCommandBuffer(MultiFenceHolder waitable, int offset, int size)
-        {
-            lock (_commandBuffers)
-            {
-                for (int i = 0; i < _totalCommandBuffers; i++)
-                {
-                    ref ReservedCommandBuffer entry = ref _commandBuffers[i];
-
-                    if (entry.InUse &&
-                        waitable.HasFence(i) &&
-                        waitable.IsBufferRangeInUse(i, offset, size))
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        public bool IsFenceOnRentedCommandBuffer(FenceHolder fence)
-        {
-            lock (_commandBuffers)
-            {
-                for (int i = 0; i < _totalCommandBuffers; i++)
-                {
-                    ref ReservedCommandBuffer entry = ref _commandBuffers[i];
-
-                    if (entry.InUse && entry.Fence == fence)
-                    {
-                        return true;
-                    }
-                }
-            }
-
-            return false;
-        }
-
-        public FenceHolder GetFence(int cbIndex)
-        {
-            return _commandBuffers[cbIndex].Fence;
-        }
-
-        public int GetSubmissionCount(int cbIndex)
-        {
-            return _commandBuffers[cbIndex].SubmissionCount;
-        }
+        public void AddDependant(int cbIndex, IAuto dependant) { dependant.IncrementReferenceCount(); _commandBuffers[cbIndex].Dependants.Add(dependant); }
+        public void AddWaitable(MultiFenceHolder waitable) { lock (_commandBuffers) { for (int i = 0; i < _totalCommandBuffers; i++) { ref ReservedCommandBuffer entry = ref _commandBuffers[i]; if (entry.InConsumption) AddWaitable(i, waitable); } } }
+        public void AddInUseWaitable(MultiFenceHolder waitable) { lock (_commandBuffers) { for (int i = 0; i < _totalCommandBuffers; i++) { ref ReservedCommandBuffer entry = ref _commandBuffers[i]; if (entry.InUse) AddWaitable(i, waitable); } } }
+        public void AddWaitable(int cbIndex, MultiFenceHolder waitable) { ref ReservedCommandBuffer entry = ref _commandBuffers[cbIndex]; if (waitable.AddFence(cbIndex, entry.Fence)) entry.Waitables.Add(waitable); }
+        public bool HasWaitableOnRentedCommandBuffer(MultiFenceHolder waitable, int offset, int size) { lock (_commandBuffers) { for (int i = 0; i < _totalCommandBuffers; i++) { ref ReservedCommandBuffer entry = ref _commandBuffers[i]; if (entry.InUse && waitable.HasFence(i) && waitable.IsBufferRangeInUse(i, offset, size)) return true; } } return false; }
+        public bool IsFenceOnRentedCommandBuffer(FenceHolder fence) { lock (_commandBuffers) { for (int i = 0; i < _totalCommandBuffers; i++) { ref ReservedCommandBuffer entry = ref _commandBuffers[i]; if (entry.InUse && entry.Fence == fence) return true; } } return false; }
+        public FenceHolder GetFence(int cbIndex) => _commandBuffers[cbIndex].Fence;
+        public int GetSubmissionCount(int cbIndex) => _commandBuffers[cbIndex].SubmissionCount;
 
         private int FreeConsumed(bool wait)
         {
             int freeEntry = 0;
-
             while (_queuedCount > 0)
             {
                 int index = _queuedIndexes[_queuedIndexesPtr];
-
                 ref ReservedCommandBuffer entry = ref _commandBuffers[index];
-
-                if (wait || !entry.InConsumption || entry.Fence.IsSignaled())
+                bool signaled = false;
+                try { signaled =!entry.InConsumption || entry.Fence.IsSignaled(); }
+                catch { FLog($"FreeConsumed cb={index} IsSignaled EXCEPTION"); }
+                FLog($"FreeConsumed cb={index} wait={wait} signaled={signaled} inUse={entry.InUse} consumption={entry.InConsumption} queued={_queuedCount} inUseCount={_inUseCount}");
+                if (wait ||!entry.InConsumption || signaled)
                 {
+                    if (entry.InConsumption &&!signaled) FLog($"FreeConsumed cb={index} -> FENCE WAIT START");
                     WaitAndDecrementRef(index);
-
+                    FLog($"FreeConsumed cb={index} -> FENCE WAIT END");
                     wait = false;
                     freeEntry = index;
-
                     _queuedCount--;
                     _queuedIndexesPtr = (_queuedIndexesPtr + 1) % _totalCommandBuffers;
                 }
-                else
-                {
-                    break;
-                }
+                else { FLog($"FreeConsumed cb={index} -> NOT READY, BREAK"); break; }
             }
-
             return freeEntry;
         }
 
-        public CommandBufferScoped ReturnAndRent(CommandBufferScoped cbs)
-        {
-            Return(cbs);
-            return Rent();
-        }
+        public CommandBufferScoped ReturnAndRent(CommandBufferScoped cbs) { Return(cbs); return Rent(); }
 
         public CommandBufferScoped Rent()
         {
             lock (_commandBuffers)
             {
-                int cursor = FreeConsumed(_inUseCount + _queuedCount == _totalCommandBuffers);
-
+                bool needWait = _inUseCount + _queuedCount == _totalCommandBuffers;
+                FLog($"RENT START inUse={_inUseCount} queued={_queuedCount} total={_totalCommandBuffers} needWait={needWait}");
+                int cursor = FreeConsumed(needWait);
+                FLog($"RENT AFTER FreeConsumed cursor={cursor} inUse={_inUseCount} queued={_queuedCount}");
                 for (int i = 0; i < _totalCommandBuffers; i++)
                 {
                     ref ReservedCommandBuffer entry = ref _commandBuffers[cursor];
-
-                    if (!entry.InUse && !entry.InConsumption)
+                    if (!entry.InUse &&!entry.InConsumption)
                     {
-                        entry.InUse = true;
-
-                        _inUseCount++;
-
-                        CommandBufferBeginInfo commandBufferBeginInfo = new()
-                        {
-                            SType = StructureType.CommandBufferBeginInfo,
-                        };
-
+                        FLog($"RENT BEGIN CB={cursor}");
+                        entry.InUse = true; _inUseCount++;
+                        CommandBufferBeginInfo commandBufferBeginInfo = new() { SType = StructureType.CommandBufferBeginInfo, };
                         _api.BeginCommandBuffer(entry.CommandBuffer, in commandBufferBeginInfo).ThrowOnError();
-
+                        FLog($"RENT OK CB={cursor}");
                         return new CommandBufferScoped(this, entry.CommandBuffer, cursor);
                     }
-
                     cursor = (cursor + 1) & _totalCommandBuffersMask;
                 }
+                FLog($"RENT OUT OF BUFFERS inUse={_inUseCount} queued={_queuedCount}");
+                throw new InvalidOperationException($"Out of command buffers (In use: {_inUseCount}, queued: {_queuedCount}, total: {_totalCommandBuffers})");
             }
-
-            throw new InvalidOperationException($"Out of command buffers (In use: {_inUseCount}, queued: {_queuedCount}, total: {_totalCommandBuffers})");
         }
 
-        public void Return(CommandBufferScoped cbs)
-        {
-            Return(cbs, null, null, null);
-        }
+        public void Return(CommandBufferScoped cbs) => Return(cbs, null, null, null);
 
-        public unsafe void Return(
-            CommandBufferScoped cbs,
-            ReadOnlySpan<Semaphore> waitSemaphores,
-            ReadOnlySpan<PipelineStageFlags> waitDstStageMask,
-            ReadOnlySpan<Semaphore> signalSemaphores)
+        public unsafe void Return(CommandBufferScoped cbs, ReadOnlySpan<Semaphore> waitSemaphores, ReadOnlySpan<PipelineStageFlags> waitDstStageMask, ReadOnlySpan<Semaphore> signalSemaphores)
         {
             lock (_commandBuffers)
             {
                 int cbIndex = cbs.CommandBufferIndex;
-
                 ref ReservedCommandBuffer entry = ref _commandBuffers[cbIndex];
-
                 Debug.Assert(entry.InUse);
                 Debug.Assert(entry.CommandBuffer.Handle == cbs.CommandBuffer.Handle);
-                entry.InUse = false;
-                entry.InConsumption = true;
-                entry.SubmissionCount++;
-                _inUseCount--;
-
+                entry.InUse = false; entry.InConsumption = true; entry.SubmissionCount++; _inUseCount--;
                 CommandBuffer commandBuffer = entry.CommandBuffer;
-
+                FLog($"RETURN cb={cbIndex} END START");
                 _api.EndCommandBuffer(commandBuffer).ThrowOnError();
-
+                FLog($"RETURN cb={cbIndex} END OK");
                 fixed (Semaphore* pWaitSemaphores = waitSemaphores, pSignalSemaphores = signalSemaphores)
                 {
                     fixed (PipelineStageFlags* pWaitDstStageMask = waitDstStageMask)
                     {
-                        SubmitInfo sInfo = new()
-                        {
-                            SType = StructureType.SubmitInfo,
-                            WaitSemaphoreCount = !waitSemaphores.IsEmpty ? (uint)waitSemaphores.Length : 0,
-                            PWaitSemaphores = pWaitSemaphores,
-                            PWaitDstStageMask = pWaitDstStageMask,
-                            CommandBufferCount = 1,
-                            PCommandBuffers = &commandBuffer,
-                            SignalSemaphoreCount = !signalSemaphores.IsEmpty ? (uint)signalSemaphores.Length : 0,
-                            PSignalSemaphores = pSignalSemaphores,
-                        };
-
+                        SubmitInfo sInfo = new() { SType = StructureType.SubmitInfo, WaitSemaphoreCount =!waitSemaphores.IsEmpty? (uint)waitSemaphores.Length : 0, PWaitSemaphores = pWaitSemaphores, PWaitDstStageMask = pWaitDstStageMask, CommandBufferCount = 1, PCommandBuffers = &commandBuffer, SignalSemaphoreCount =!signalSemaphores.IsEmpty? (uint)signalSemaphores.Length : 0, PSignalSemaphores = pSignalSemaphores, };
                         lock (_queueLock)
                         {
+                            FLog($"RETURN cb={cbIndex} QUEUE SUBMIT START");
                             _api.QueueSubmit(_queue, 1, in sInfo, entry.Fence.GetUnsafe()).ThrowOnError();
+                            FLog($"RETURN cb={cbIndex} QUEUE SUBMIT OK");
                         }
                     }
                 }
-
                 int ptr = (_queuedIndexesPtr + _queuedCount) % _totalCommandBuffers;
-                _queuedIndexes[ptr] = cbIndex;
-                _queuedCount++;
+                _queuedIndexes[ptr] = cbIndex; _queuedCount++;
             }
         }
 
         private void WaitAndDecrementRef(int cbIndex, bool refreshFence = true)
         {
             ref ReservedCommandBuffer entry = ref _commandBuffers[cbIndex];
-
             if (entry.InConsumption)
             {
                 entry.Fence.Wait();
                 entry.InConsumption = false;
             }
-
-            foreach (IAuto dependant in entry.Dependants)
-            {
-                dependant.DecrementReferenceCount(cbIndex);
-            }
-
-            foreach (MultiFenceHolder waitable in entry.Waitables)
-            {
-                waitable.RemoveFence(cbIndex);
-                waitable.RemoveBufferUses(cbIndex);
-            }
-
-            entry.Dependants.Clear();
-            entry.Waitables.Clear();
+            foreach (IAuto dependant in entry.Dependants) dependant.DecrementReferenceCount(cbIndex);
+            foreach (MultiFenceHolder waitable in entry.Waitables) { waitable.RemoveFence(cbIndex); waitable.RemoveBufferUses(cbIndex); }
+            entry.Dependants.Clear(); entry.Waitables.Clear();
             entry.Fence?.Dispose();
-
-            if (refreshFence)
-            {
-                entry.Fence = new FenceHolder(_api, _device, _concurrentFenceWaitUnsupported);
-            }
-            else
-            {
-                entry.Fence = null;
-            }
+            if (refreshFence) entry.Fence = new FenceHolder(_api, _device, _concurrentFenceWaitUnsupported); else entry.Fence = null;
         }
 
         public unsafe void Dispose()
         {
-            for (int i = 0; i < _totalCommandBuffers; i++)
-            {
-                WaitAndDecrementRef(i, refreshFence: false);
-            }
-
+            for (int i = 0; i < _totalCommandBuffers; i++) WaitAndDecrementRef(i, refreshFence: false);
             _api.DestroyCommandPool(_device, _pool, null);
         }
     }
